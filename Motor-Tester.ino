31ac9603220d38f28b8fd8c211186100be5851fb
Sakib Ahmed Sumdany <ahmadSum1@users.noreply.github.com>
2016-11-19 15:43:20 +0600

    Minor typo corrections

--- Motor-Tester.ino
+++ Motor-Tester.ino
@@ -3,7 +3,6 @@ Author: M M Shaifur Rahman
 Created Date: 11 Nov 2016
 Web: http://shaifur.me
 E-mail: mail@shaifur.me
-
 Connection Diagram:
 Arduino <-->  Motor Driver
   D2    <--> MotorA1
@@ -12,7 +11,6 @@ Arduino <-->  Motor Driver
   D5    <--> MotorB1
   D7    <--> MotorB2
   D6    <--> MotorB-PWM
-
 Description:
 This code will help you to test any L293D or L298N based standard Motor Driver.This default code will drive your every possible combination. Use this functions and do further modification to make it work in your real project.
 */
@@ -23,7 +21,7 @@ byte LeftMSpeed = 3;
 
 byte RightMotorA = 5;
 byte RightMotorB = 7;
-byte RightMSpeed = 6
+byte RightMSpeed = 6;
 
 void setup() 
 {
@@ -32,14 +30,14 @@ Serial.begin(9600);
 pinMode(2,OUTPUT);
 pinMode(3,OUTPUT);
 pinMode(4,OUTPUT);
-pinMode(5,OUTPTU);
+pinMode(5,OUTPUT);
 pinMode(6,OUTPUT);
 
 }
 
 void loop() 
 {
-motor_Speed(); // Set Motor Speed
+MotorSpeed(); // Set Motor Speed
 Serial.println("Driving Forward");
 drive_forward();
 delay(1000);
@@ -68,54 +66,47 @@ delay(2000);
 
 // Set Motor Speed 0 is stop & 255 is max speed
 void MotorSpeed(){
-analogWrite(leftMotorSpeed, 255);
-analogWrite(RightMotorSpeed,255);
+analogWrite(LeftMSpeed, 255);
+analogWrite(RightMSpeed,255);
 }
 //Stop Both Motor
 void motor_stop(){
-digitalWrite(leftMotorA, LOW); 
-digitalWrite(leftMotorB, LOW); 
+digitalWrite(LeftMotorA, LOW); 
+digitalWrite(LeftMotorB, LOW); 
 digitalWrite(RightMotorA, LOW); 
 digitalWrite(RightMotorB, LOW);
 delay(5);
 }
 
 void drive_forward(){
-digitalWrite(leftMotorA, HIGH); 
-digitalWrite(leftMotorB, LOW); 
+digitalWrite(LeftMotorA, HIGH); 
+digitalWrite(LeftMotorB, LOW); 
 digitalWrite(RightMotorA, HIGH); 
 digitalWrite(RightMotorB, LOW);
 delay(5);
 }
 
 void drive_backward(){
-digitalWrite(leftMotorA, LOW); 
-digitalWrite(leftMotorB, HIGH); 
+digitalWrite(LeftMotorA, LOW); 
+digitalWrite(LeftMotorB, HIGH); 
 digitalWrite(RightMotorA, LOW); 
 digitalWrite(RightMotorB, HIGH);
 delay(5); 
 }
 
 void turn_left(){
-digitalWrite(leftMotorA, LOW); 
-digitalWrite(leftMotorB, LOW); 
+digitalWrite(LeftMotorA, LOW); 
+digitalWrite(LeftMotorB, LOW); 
 digitalWrite(RightMotorA, HIGH); 
 digitalWrite(RightMotorB, LOW);
 delay(5); 
 }
 
 void turn_right(){
-digitalWrite(leftMotorA, HIGH); 
-digitalWrite(leftMotorB, LOW); 
+digitalWrite(LeftMotorA, HIGH); 
+digitalWrite(LeftMotorB, LOW); 
 digitalWrite(RightMotorA, LOW); 
 digitalWrite(RightMotorB, LOW);
 delay(5); 
 }
 //This is function is not used in Testing.
-void turn360(){
-digitalWrite(leftMotorA, HIGH); 
-digitalWrite(leftMotorB, LOW); 
-digitalWrite(RightMotorA, LOW); 
-digitalWrite(RightMotorB, HIGH);
-delay(5);
-}

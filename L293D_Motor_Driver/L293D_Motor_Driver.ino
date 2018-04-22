7fb2973966d93c47267359053fd228877205aed8
Shaifur Rahman <shaifur.cse@gmail.com>
2018-04-22 09:01:40 +0600

    veriable updated
    variable name changed, description changed. version added

--- L293D_Motor_Driver/L293D_Motor_Driver.ino
+++ L293D_Motor_Driver/L293D_Motor_Driver.ino
@@ -1,111 +1,110 @@
 /*
-Author: M M Shaifur Rahman
-Created Date: 11 Nov 2016
-Web: http://shaifur.me
-E-mail: mail@shaifur.me
-Connection Diagram:
-Arduino <-->  Motor Driver
+  Written by    : Shaifur Rahman
+  Date          : 01 April 2018
+  Device(s)     : Arduino UNO, TekModules Digital IR 8Array
+  Code Version  : 2.0
+
+  Connection Diagram:
+  Arduino <-->  Motor Driver
   D2    <--> MotorA1
   D4    <--> MotorA2
   D3    <--> MotorA-PWM
   D5    <--> MotorB1
   D7    <--> MotorB2
   D6    <--> MotorB-PWM
-Description:
-This code will help you to test any L293D or L298N based standard Motor Driver.This default code will drive your every possible combination. Use this functions and do further modification to make it work in your real project.
+  Description:
+  This code will help you to test any L293D or L298N based standard Motor Driver.This default code will drive your motor every possible combination. Use this functions and do further modification to make it work in your real projects.
 */
 
-byte LeftMotorA = 2;
-byte LeftMotorB = 4;
-byte LeftMSpeed = 3; 
+byte motorA_1 = 2;
+byte motorA_2 = 4;
+byte motorA_Speed = 3;
+byte motorB_1 = 5;
+byte motorB_2 = 7;
+byte motorB_Speed = 6;
 
-byte RightMotorA = 5;
-byte RightMotorB = 7;
-byte RightMSpeed = 6;
-
-void setup() 
+void setup()
 {
-Serial.begin(9600);
-//Initialized Motor Driver Pins as Output
-pinMode(2,OUTPUT);
-pinMode(3,OUTPUT);
-pinMode(4,OUTPUT);
-pinMode(5,OUTPUT);
-pinMode(6,OUTPUT);
-
+  Serial.begin(9600);
+  //Initialized Motor Driver Pins as Output
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
 }
 
-void loop() 
+void loop()
 {
-MotorSpeed(); // Set Motor Speed
-Serial.println("Driving Forward");
-drive_forward();
-delay(1000);
-motor_stop();
+  MotorSpeed(); // Set Motor Speed
+  Serial.println("Driving Forward");
+  drive_forward();
+  delay(1000);
+  motor_stop();
 
-Serial.println("Driving Forward");
-drive_backward();
-delay(1000);
-motor_stop();
+  Serial.println("Driving Forward");
+  drive_backward();
+  delay(1000);
+  motor_stop();
 
-Serial.println("Turnning Left");
-turn_left();
-delay(1000);
-motor_stop();
+  Serial.println("Turnning Left");
+  turn_left();
+  delay(1000);
+  motor_stop();
 
-Serial.println("Turnning Left");
-turn_right();
-delay(1000);
-motor_stop();
+  Serial.println("Turnning Left");
+  turn_right();
+  delay(1000);
+  motor_stop();
 
-Serial.println("Testing Done !");
-delay(2000);
+  Serial.println("Testing Done !");
+  delay(2000);
 }
 
 // Motor Driving Functions
 
 // Set Motor Speed 0 is stop & 255 is max speed
-void MotorSpeed(){
-analogWrite(LeftMSpeed, 255);
-analogWrite(RightMSpeed,255);
+void MotorSpeed() {
+  analogWrite(motorA_Speed, 255);
+  analogWrite(motorB_Speed, 255);
 }
 //Stop Both Motor
-void motor_stop(){
-digitalWrite(LeftMotorA, LOW); 
-digitalWrite(LeftMotorB, LOW); 
-digitalWrite(RightMotorA, LOW); 
-digitalWrite(RightMotorB, LOW);
-delay(5);
+void motor_stop() {
+  digitalWrite(motorA_1, LOW);
+  digitalWrite(motorA_2, LOW);
+  digitalWrite(motorB_1, LOW);
+  digitalWrite(motorB_2, LOW);
+  delay(5);
 }
 
-void drive_forward(){
-digitalWrite(LeftMotorA, HIGH); 
-digitalWrite(LeftMotorB, LOW); 
-digitalWrite(RightMotorA, HIGH); 
-digitalWrite(RightMotorB, LOW);
-delay(5);
+void drive_forward() {
+  digitalWrite(motorA_1, HIGH);
+  digitalWrite(motorA_2, LOW);
+  digitalWrite(motorB_1, HIGH);
+  digitalWrite(motorB_2, LOW);
+  delay(5);
 }
 
-void drive_backward(){
-digitalWrite(LeftMotorA, LOW); 
-digitalWrite(LeftMotorB, HIGH); 
-digitalWrite(RightMotorA, LOW); 
-digitalWrite(RightMotorB, HIGH);
-delay(5); 
+void drive_backward() {
+  digitalWrite(motorA_1, LOW);
+  digitalWrite(motorA_2, HIGH);
+  digitalWrite(motorB_1, LOW);
+  digitalWrite(motorB_2, HIGH);
+  delay(5);
 }
 
-void turn_left(){
-digitalWrite(LeftMotorA, LOW); 
-digitalWrite(LeftMotorB, LOW); 
-digitalWrite(RightMotorA, HIGH); 
-digitalWrite(RightMotorB, LOW);
-delay(5); 
+void turn_left() {
+  digitalWrite(motorA_1, LOW);
+  digitalWrite(motorA_2, LOW);
+  digitalWrite(motorB_1, HIGH);
+  digitalWrite(motorB_2, LOW);
+  delay(5);
 }
 
-void turn_right(){
-digitalWrite(LeftMotorA, HIGH); 
-digitalWrite(LeftMotorB, LOW); 
-digitalWrite(RightMotorA, LOW); 
-digitalWrite(RightMotorB, LOW);
-delay(5); 
+void turn_right() {
+  digitalWrite(motorA_1, HIGH);
+  digitalWrite(motorA_2, LOW);
+  digitalWrite(motorB_1, LOW);
+  digitalWrite(motorB_2, LOW);
+  delay(5);
 }
